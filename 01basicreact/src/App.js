2494978b33c6a9f00ca8a0b55f5a8fb055023421
Rohitmotghare <128033412+Rohitmotghare@users.noreply.github.com>
2023-10-06 15:08:09 +0530

    updates on learning

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,13 @@
+
+import Chai from "./Chai";
+
+function App() {
+  return (
+    <> 
+     <Chai/> 
+     <h1>starting react</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/chai.jsx
@@ -0,0 +1,8 @@
+function Chai(){
+    return(
+        <h3>chai is reardy</h3>
+        
+    )
+}
+
+export default Chai
