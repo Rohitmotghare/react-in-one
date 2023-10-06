2a37a9661cf99e86f72035e1e7e1c257632bf2f1
Rohitmotghare <128033412+Rohitmotghare@users.noreply.github.com>
2023-10-06 17:37:15 +0530

    updates in learning

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,12 +1,14 @@
 import Chai from "./chai"
 
 function App() {
+
+  const username = "chai or code"
   
 
   return (
     <> 
       <Chai/>
-      <h1>chai or react</h1>
+      <h1>chai or react {username}</h1>
       <p>test para</p>
     </>  
   )
